f86fcce7bdfea20356eb2b2ce9dc1563b9a21de7
Vitah <linw1225@gmail.com>
2019-05-30 17:04:25 +0800

    Feat：HelloWorld

---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function HelloWorld() {
+    return ReactDOM.render(
+        <h1>Hello world</h1>,
+        document.getElementById('root')
+    )
+}
+
+export default HelloWorld;
