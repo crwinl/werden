2973e8f8764662353d3f5df8a38bff939daaa321
Vitah <linw1225@gmail.com>
2019-05-30 17:59:06 +0800

    Refactor：示例代码注释

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import HelloWorld from './demo/HelloWorld';
+import App from './App';
 
+/**
+ * for demo
+ * 
+import HelloWorld from './demo/HelloWorld';
+ */
 
 import * as serviceWorker from './serviceWorker';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+/**
+ * for demo
+ *
+//ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ */
