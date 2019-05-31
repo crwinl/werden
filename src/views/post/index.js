b451154310ae4213ff53162ca22a4a7aac2fd9d0
Vitah <linw1225@gmail.com>
2019-05-31 14:32:24 +0800

    Feat：Post详情页

---
+++ src/views/post/index.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+
+class PostDetail extends React.Component {
+
+    state = {
+        title: '',
+        content: ''
+    }
+
+    // 方法会在组件已经呗渲染到DOM后运行
+    componentDidMount() {
+        this.fetchPost();
+    }
+
+    componentWillUnmount() {
+    }
+
+    fetchPost() {
+        this.setState({
+            title: "test title",
+            content: "test content"
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.title}</h1>
+                <h1>{this.state.content}</h1>
+            </div>
+        );
+    }
+}
+
+export default PostDetail
