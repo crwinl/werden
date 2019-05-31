4c3ae89787f9895f0ebd6b087df350b2c8ef548c
Vitah <linw1225@gmail.com>
2019-05-31 15:30:39 +0800

    Feat：界面跳转

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+import logo from './logo.svg';
+
+import PostDetail from './views/post/index';
 
 function App() {
+  function handleClick(e) {
+    ReactDOM.render(<PostDetail />, document.getElementById('root'));
+  }
+
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </p>
+        <a href="#/postdetail" onClick={handleClick} rel="noopener noreferrer">
+          Click me
+    </a>
       </header>
     </div>
+
+
   );
 }
 
