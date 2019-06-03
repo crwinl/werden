a76eab1b5b33c4225cbcc869f061ad2d86355dbe
Vitah <linw1225@gmail.com>
2019-06-03 17:05:15 +0800

    Feat：主页文章内容

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -7,7 +7,9 @@ import {
 
 import {
     Empty,
+    Divider
 } from 'antd';
+
 
 class Home extends Component {
 
@@ -18,6 +20,7 @@ class Home extends Component {
     }
 
     componentDidMount() {
+        this.fetchPostList();
     }
 
     componentWillReceiveProps() {
@@ -25,7 +28,13 @@ class Home extends Component {
 
     // 获取文章列表数据
     fetchPostList() {
-        var post_list = [];
+        var post_list = [{
+            id: 1,
+            'title': "aajaj",
+            'content': "ajajajajajaj",
+            'created_at': "19992-12-25"
+        }];
+
         this.setState({
             post_list: post_list
         });
@@ -34,6 +43,8 @@ class Home extends Component {
     }
 
     render() {
+        const post_list = this.state.post_list;
+
         // 列表为空的显示内容
         if (this.state.post_list.length === 0) {
             return (
@@ -46,6 +57,26 @@ class Home extends Component {
         return (
             <div className="content-inner-wrapper home">
                 <Fragment>
+                    <ul className="ul-list">
+                        {
+                            post_list.map(post => (
+                                <li key={post.id} className="ul-list-item">
+                                    <Divider>
+                                        <span className="title">
+                                            {post.title}
+                                        </span>
+                                        <span className="create-time">
+                                            {post.created_at}
+                                        </span>
+                                    </Divider>
+
+                                    <div className="post-detail">
+                                        {post.content}
+                                    </div>
+                                </li>
+                            ))
+                        }
+                    </ul>
                 </Fragment>
             </div>
         );
