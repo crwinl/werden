1fc729ed0bc0df231fea62789b7ba2f9eb61b5f5
Vitah <linw1225@gmail.com>
2019-06-11 15:18:32 +0800

    Refactor：删除无用代码

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,44 +6,19 @@ import {
     Col
 } from 'antd';
 
-const Header = Layout.Header;
-
-const navList = [
-    {
-        icon: 'home',
-        title: '首页',
-        link: '/'
-    },
-    {
-        icon: 'edit',
-        title: '归档',
-        link: '/archives'
-    },
-    {
-        icon: 'folder',
-        title: '分类',
-        link: '/categories'
-    },
-    {
-        icon: 'user',
-        title: '关于',
-        link: '/about'
-    }
-]
-
 const BlogHeader = () => {
     const responsiveLeft = { xxl: 4, xl: 5, lg: 5, sm: 4, xs: 24 }
     const responsiveRight = { xxl: 20, xl: 19, lg: 19, sm: 20, xs: 0 }
 
     return (
-        <Header className="header-container">
+        <Layout.Header className="header-container">
             <Row>
                 <Col {...responsiveLeft}>
                 </Col>
                 <Col {...responsiveRight}>
                 </Col>
             </Row>
-        </Header>
+        </Layout.Header>
     )
 }
 
