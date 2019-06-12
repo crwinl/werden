763a906db4f302bbff2de39c9b235ed7a7255497
Vitah <linw1225@gmail.com>
2019-06-12 17:56:39 +0800

    Feat：修改Header样式

--- src/components/header/headerLeft.jsx
+++ src/components/header/headerLeft.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeaderLeft = () => {
     return (
         <div className="header-left">
-            <span className="blog-name">郭大大的博客</span>
+            <span className="blog-name">这是博客</span>
         </div>
     )
 }
